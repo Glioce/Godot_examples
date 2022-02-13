564b05d7cfd3d47700766d5b712a024b213bb0b0
a <a>
2022-02-13 03:14:59 -0600

    collisions using predefined objects

--- n19_00/Scripts/Player.gd
+++ n19_00/Scripts/Player.gd
@@ -1,10 +1,11 @@
-extends Area2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var v = 2 # walking speed
+var r = 3*60 # walking speed
+var v = Vector2() # velocity
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,11 +19,25 @@ func _ready():
 
 
 func _physics_process(_delta):
+	v = Vector2(0, 0) # reset velocity
+	
 	if Input.is_action_pressed("ui_right"):
-		self.position.x += v
+		v.x += 1
 	if Input.is_action_pressed("ui_left"):
-		self.position.x -= v
+		v.x -= 1
 	if Input.is_action_pressed("ui_down"):
-		self.position.y += v
+		v.y += 1
 	if Input.is_action_pressed("ui_up"):
-		self.position.y -= v
+		v.y -= 1
+	
+	v = v.normalized()
+	move_and_slide(v * r)
+	
+#	if Input.is_action_pressed("ui_right"):
+#		self.position.x += v
+#	if Input.is_action_pressed("ui_left"):
+#		self.position.x -= v
+#	if Input.is_action_pressed("ui_down"):
+#		self.position.y += v
+#	if Input.is_action_pressed("ui_up"):
+#		self.position.y -= v
