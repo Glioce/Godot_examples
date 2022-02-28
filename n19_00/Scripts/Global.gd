0dbb2308b2346a7e179e2b08f6870435f0a8c313
a <a>
2022-02-27 22:08:17 -0600

    add Global singleton and change main scene

---
+++ n19_00/Scripts/Global.gd
@@ -0,0 +1,15 @@
+extends Node
+# This script is auto-loaded (Project Settings)
+# Contains global variables used in different scenes
+
+# Test variable
+var terrible = true
+
+# Wether the player is doing scene transition or not
+var scene_transition = false
+
+# The position the player moves when entering the new scene
+var transition_pos = Vector2()
+
+# Sscene size in pixels (can be obtained from a tile map)
+var scene_size = Vector2()
