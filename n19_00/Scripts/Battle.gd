bf5b76c3c4bcb8f8280d70eba9bbc61a9b6db0ce
a <a>
2022-03-25 08:16:56 -0600

    building battle scene

--- n19_00/Scripts/Battle.gd
+++ n19_00/Scripts/Battle.gd
@@ -1,16 +1,38 @@
 extends Node2D
 
+# States
+enum S {FADE_IN, FADE_OUT, GUI, SEL_MOVE, SEL_AREA, MOVE_ANIM, END}
+# FADE_IN - Intro animation, Player and Piñata fade in, text on the bottom
+# FADE_OUT - Player and Piñata fade out, text dont move
+# GUI - The GUI appears
+# SEL_MOVE - Select move or attack
+# SEL_AREA - Select area to attack
+# MOVE_ANIM - Animation of the attack
+# END - The player win or fail
+var state = S.FADE_IN
+var state_begin := true # To trigger animations just one time
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var animation_player := $AnimationPlayer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("Intro")
+	#animation_player.play("Intro")
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	match state:
+		S.FADE_IN:
+			if state_begin:
+				animation_player.play("Intro")
+				state_begin = false
+		S.FADE_OUT:
+			if state_begin:
+				animation_player.play("Intro", 1, 1, true)
+				state_begin = false
+	
+	pass
+
+
+func _on_TouchScreenButton_pressed():
+	print("TOUCH")
+	pass # Replace with function body.
