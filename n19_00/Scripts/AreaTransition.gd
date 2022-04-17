8b54538dc3e5a5ce68419cacb36a850a08500b0f
a <a>
2022-04-17 15:55:16 -0500

    comments

--- n19_00/Scripts/AreaTransition.gd
+++ n19_00/Scripts/AreaTransition.gd
@@ -1,3 +1,32 @@
+# Trasition
+#
+# Puertas en las casas y en las orillas de los mapas.
+# Se usa para indicar las zonas por donde se puede acceder a otra escena.
+# body entered signal para guardar identificador de objeto
+# body exited signal para borrar identificador de objeto
+# se puede rotar para indicar la dirección de salida
+#
+#
+#Antes de cambiar de escena guarda:
+#- coordenadas relativas al objeto puerta
+#- nombre del objeto destino en la nueva escena (o nombre de grupo)
+#
+#En la nueva escena el Player revisa si se realizó una transición en Global
+#Global.scene_transition == true
+#
+#Si ocurre la transición
+#- Busca el objeto puerta en el que debe iniciar
+#- Se transporta al objeto en las coordenadas relativas
+#- Se agrega un punto a path para indicar la posición en la que termina la animación de transición
+#  El punto se agrega en dirección contraria al vector de salida (exit_vector)
+#- Player inicia en el estado TRANSITION
+#- Se desactiva la variable que indica la transisición de escena (Global.scene_transition)
+#
+#Tal vez es mejor usar los estados enter y exit. Por ahora el estado transition ha funcionado
+#
+#func _ready():
+#    add_to_group("guards")
+
 extends Area2D
 
 # Path to scene
@@ -13,13 +42,11 @@ var radians = deg2rad(degrees)
 var exit_vector := Vector2(cos(radians), -sin(radians))
 #var exit_vector := Vector2(1, 0).rotated(radians)
 
-
 func _ready():
 	print(target_scene)
 	print(exit_vector)
 	#print(degrees)
 	#print(radians)
-
 
 func _on_Area2D_body_entered(body):
 	# Set object door
@@ -30,11 +57,9 @@ func _on_Area2D_body_entered(body):
 	#body.state = body.S.PAUSE
 	#get_tree().change_scene(scene_path)
 
-
 func _on_Area2D_body_exited(body):
 	# Delete value of door
 	body.door = null
-
 
 # Project the vector door-to-player on the exit vector using dot product
 # The result is how near is the player to the scene border
--- n19_00/Scripts/Nav.gd
+++ n19_00/Scripts/Nav.gd
@@ -1,8 +1,28 @@
+# Navigation "Nav"
+# Detects the click on the map and request the path for the plaer.
+# It is done here because the map is independent of the player and 
+# the map is a children of this node.
+#
+# Usar un TileMap especial para las colisiones
+# Nombres que se pueden usar
+# - CollisionMap
+# - ColMap
+# - TileCollisions
+# - TileCol
+# - TileMapCollision
+#
+# La construcción del escenario se puede hacer en capas.
+# La mayor parte está construída con tile maps. Otras partes con sprites.
+#
+# Por lo general se deben tener 2 capas: Fondo (piso) y construcciones 
+# (obstáculos). Una capa extra es para las colisiones (mencionada anteriomente)
+#
+# Se usan nodos Area2D para detectar colisiones con el Player y cambiar 
+# de escena.
+
 extends Node2D
 
-
 var map = null
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,6 +40,7 @@ func _unhandled_input(event):
 				var path = $TileMapCol.calculate_path($Player.position, global_mouse_pos)
 				#var path = $TileMapCol.calculate_path($Player.position, event.position)
 				#var path = $Navigation2D.get_simple_path($Player.position, event.position, false)
+				$Line2D.clear_points()
 				$Line2D.points = path
 				$Player.path = path
 				$Player.state = $Player.S.FOLLOW
